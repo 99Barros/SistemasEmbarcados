09eb0869d17ffc8ec5940e361e32d933f7d2914e
99Barros <110702690+99Barros@users.noreply.github.com>
2024-09-03 14:47:27 -0300

    Update dht11.cpp

--- dht11.cpp
+++ dht11.cpp
@@ -2,16 +2,16 @@
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-byte bodyupRight[8] = {
-    B00000,
-    B01111,
-    B01001,
-    B01111,
-    B00000,
-    B00000,
-    B01000,
-    B11000,
-};
+// byte bodyupRight[8] = {
+//     B00000,
+//     B01111,
+//     B01001,
+//     B01111,
+//     B00000,
+//     B00000,
+//     B01000,
+//     B11000,
+// };
 
 #define DHTPIN 2     
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
@@ -43,9 +43,9 @@ void loop() {
   Serial.println(F("°C "));
 
   lcd.setCursor(0,0);
-  lcd.print("Temp: "+ String(temperature) + "°C");
+  lcd.print("Temp: "+ String(temperature) + " C");
   lcd.setCursor(0,1);
-  lcd.print("Humi: "+ String(humidity) + "%");
+  lcd.print("Humi: "+ String(humidity) + " %");
 
   // Wait a few seconds between measurements.
   delay(2000);
